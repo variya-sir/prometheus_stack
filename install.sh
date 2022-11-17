fc3f7eb76858079b118a3dac104c947473d6831f
bell-twm <variya.sirilertworakul@motional.com>
2022-11-17 10:53:12 +0700

    chore: add todo

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ echo "Setup local development"
 echo "Create .env file"
 cp .env.example .env
 
+## TODO: fix env inputs for alertmanager
 echo "Export secret variables from .env to .yml files"
 set -a ; source .env; envsubst < alertmanager/alertmanager.yml.in > alertmanager/alertmanager.yml
